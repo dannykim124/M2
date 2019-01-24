b8c9ef994e83f286d9d478c8aa456381d94b5bba
Roshan Siwakoti <rsiwakoti6@gatech.edu>
2019-01-24 17:53:08 -0500

    I updated my full name on the AntLabMain.java

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -65,7 +65,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p2.toString("gburdell2");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("Roshan");
+		  Person3 p3 = new Person3("Roshan Siwakoti");
 		  toPrint += p3.toString("rsiwakoti6");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
